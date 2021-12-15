990a85dcd9f4173680c6590e9d17aa5c8fdae3fc
Sanjeeban <sanjeeban5644@gmail.com>
2021-12-15 09:34:18 +0530

    tenth commit

--- circular_queue.c
+++ circular_queue.c
@@ -5,7 +5,8 @@ void enqueue();
 void dequeue();
 void display();
 
-int front=-1,rear=-1;
+int front=-1,rear=-1,final=0;
+char ch;
 
 int main()
 {
@@ -25,16 +26,95 @@ int main()
         scanf("%d",&choice);
         switch(choice)
         {
-            case 1 : enqueue(queue); break;
-            case 2 : dequeue(queue); break;
-            case 3 : display(queue); break;
-            default : printf("\nProgram Ended\n\nThankyou!!"); exit(2);
+            case 1 : enqueue(queue,size); break;
+            case 2 : dequeue(queue,size); break;
+            case 3 : display(queue,size); break;
+            default : final=1; display(queue,size) ; printf("\nProgram Ended\n\nThankyou!!"); exit(2);
         }
     }
     return 0;
 }
 
-void enqueue(int queue[])
+void enqueue(int queue[],int size)
 {
-    
+    int ele;
+    printf("\n--Entering the data--");
+    printf("\nEnter the data : ");
+    scanf("%d",&ele);
+    if(rear==-1)
+    {
+        front=0;
+        rear=0;
+        queue[rear]=ele;
+    }
+    else if(((rear+1)%size)==front)
+    {
+        printf("\n--Overflow--");
+        printf("\nPress 'y' to continue : ");
+        scanf(" %c",&ch);
+        return ;
+    }
+    else
+    {
+        rear=(rear+1)%size;
+        queue[rear]=ele;
+    }
+    printf("\nElement inserted successfully!");
+    printf("\nPress 'y' to continue : ");
+    scanf(" %c",&ch);
+}
+
+void dequeue(int queue[],int size)
+{
+    if(front==-1 && rear==-1)
+    {
+        printf("\n--UNDERFLOW--");
+        printf("\nPress 'y' to continue : ");
+        scanf(" %c",&ch);
+        return ;
+    }
+    else if(front == rear)
+    {
+        printf("\nThe element deleted is : %d",queue[front]);
+        front=-1;
+        rear=-1;
+    }
+    else
+    {
+        printf("\nThe element deleted is : %d",queue[front]);
+        front=(front+1)%size;
+    }
+    printf("\nPress 'y' to continue : ");
+    scanf(" %c",&ch);
+}
+
+void display(int queue[],int size)
+{
+    int i=front;
+    if(front==-1 && rear==-1)
+    {
+        printf("\n--QUEUE IS EMPTY--");
+        printf("\nPress 'y' to continue : ");
+        scanf(" %c",&ch);
+        return ;
+    }
+    else
+    {
+        if(final==0)
+        {
+            printf("\nQueue is : ");
+        }
+        else
+        {
+            printf("\nThe Final Queue is : ");
+        }
+        while(i!=rear)
+        {
+            printf(" %d,",queue[i]);
+            i=(i+1)%size;
+        }
+        printf(" %d.",queue[i]);
+        printf("\nPress 'y' to continue : ");
+        scanf(" %c",&ch);
+    }
 }
